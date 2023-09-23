8931cfcff39707850494e67f6bdf2959a7cedb5f
Strikelab <contact@strikelab.fr>
2023-09-23 21:08:21 +0200

    BugFix form success message

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -4,7 +4,10 @@ import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
 import Button, { BUTTON_TYPES } from "../../components/Button";
 
-const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 1000); })
+const mockContactApi = () =>
+  new Promise((resolve) => {
+    setTimeout(resolve, 1000);
+  });
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
@@ -16,6 +19,10 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
+        // Error :
+        // When Form is created › and a click is triggered on the submit button › the success message is displayed
+        // we need to call onSuccess function when form is posted
+        onSuccess();
       } catch (err) {
         setSending(false);
         onError(err);
@@ -56,11 +63,11 @@ const Form = ({ onSuccess, onError }) => {
 Form.propTypes = {
   onError: PropTypes.func,
   onSuccess: PropTypes.func,
-}
+};
 
 Form.defaultProps = {
   onError: () => null,
   onSuccess: () => null,
-}
+};
 
 export default Form;
