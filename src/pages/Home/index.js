63e855795c354be1a26957742c8680c5af76d6b1
Strikelab <contact@strikelab.fr>
2023-09-24 15:23:05 +0200

    BugFix navigation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -24,7 +24,8 @@ const Page = () => {
         <section className="SliderContainer">
           <Slider />
         </section>
-        <section className="ServicesContainer">
+        {/* anchor Menu */}
+        <section id="nos-services" className="ServicesContainer">
           <h2 className="Title">Nos services</h2>
           <p>Nous organisons des événements sur mesure partout dans le monde</p>
           <div className="ListContainer">
@@ -53,11 +54,13 @@ const Page = () => {
             </ServiceCard>
           </div>
         </section>
-        <section className="EventsContainer">
+        {/* anchor Menu */}
+        <section id="nos-realisations" className="EventsContainer">
           <h2 className="Title">Nos réalisations</h2>
           <EventList />
         </section>
-        <section className="PeoplesContainer">
+        {/* anchor Menu */}
+        <section id="notre-equipe" className="PeoplesContainer">
           <h2 className="Title">Notre équipe</h2>
           <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
           <div className="ListContainer">
