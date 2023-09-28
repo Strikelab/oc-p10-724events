c077066efa6c41855fe6dcf93aad27622100e5b0
Strikelab <contact@strikelab.fr>
2023-09-28 11:38:51 +0200

    BugFix Date V2 most logic way is to correct collection keys

--- src/helpers/Date/index.js
+++ src/helpers/Date/index.js
@@ -1,20 +1,18 @@
+// date.getMonth start janvier : 0
+// changes :
 export const MONTHS = {
-  1: "janvier",
-  2: "février",
-  3: "mars",
-  4: "avril",
-  5: "mai",
-  6: "juin",
-  7: "juillet",
-  8: "août",
-  9: "septembre",
-  10: "octobre",
-  11: "novembre",
-  12: "décembre",
+  0: "janvier",
+  1: "février",
+  2: "mars",
+  3: "avril",
+  4: "mai",
+  5: "juin",
+  6: "juillet",
+  7: "août",
+  8: "septembre",
+  9: "octobre",
+  10: "novembre",
+  11: "décembre",
 };
 
-// date.getMonth start janvier : 0
-// Original :
-// export const getMonth = (date) => MONTHS[date.getMonth()];
-// changes :
-export const getMonth = (date) => MONTHS[date.getMonth() + 1];
+export const getMonth = (date) => MONTHS[date.getMonth()];
