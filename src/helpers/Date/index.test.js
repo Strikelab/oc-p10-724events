0b9a739ba1c03f670603414a3abca7512e9b13bd
Strikelab <contact@strikelab.fr>
2023-09-29 11:24:16 +0200

    Test suite date adding some tests

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,15 +1,20 @@
-/**
- * 
- */
+import { getMonth } from ".";
 
 describe("Date helper", () => {
-    describe("When getMonth is called", () => {
-        it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
-        });
-        it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
-        });
+  describe("When getMonth is called", () => {
+    it("the function return janvier for 2022-01-01 as date", () => {
+      // Test date implementation
+      const date = "2022-01-01";
+      const month = "janvier";
+      const result = getMonth(new Date(date));
+      expect(result).toEqual(month);
     });
-})
-
+    it("the function return juillet for 2022-07-08 as date", () => {
+      // Test date implementation
+      const date = "2022-07-08";
+      const month = "juillet";
+      const result = getMonth(new Date(date));
+      expect(result).toEqual(month);
+    });
+  });
+});
